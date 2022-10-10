ec45bbb6c925470e0eaaad615d218d386a0f5fc4
Vlad Kiselev <vladk-target@yandex.ru>
2022-10-10 10:18:21 +0300

    users complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.scss';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
@@ -6,10 +6,55 @@ import { Users } from './components/Users';
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [searchValue, setSearchValue] = useState('')
+  const [invites, setInvetes] = useState([])
+  const [success, setSucces] = useState(false)
+
+
+  useEffect(() =>  {
+    fetch('https://reqres.in/api/users')
+    .then(res => res.json())
+    .then(json => {
+      setUsers(json.data)
+    })
+    .catch(err => {
+      console.warn(err)
+      alert('Ошибка при получении пользователей')
+    }).finally(() => setIsLoading(false))
+  },[])
+
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value)
+  }
+
+  const onClickInvite = (id) => {
+    if(invites.includes(id)){
+      setInvetes(prev => prev.filter(_id => _id !== id))
+    } else {
+      setInvetes(prev => [...prev, id])
+    }
+  }
+
+  const onClickSendInvites = () => {
+    setSucces(true)
+  }
+
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+    {success ? 
+    <Success count = {invites.length} /> 
+    : 
+    <Users 
+      onChangeSearchValue = {onChangeSearchValue} 
+      searchValue = {searchValue} 
+      items = {users} 
+      isLoading = {isLoading}
+      invites = {invites}
+      onClickInvite = {onClickInvite}
+      onClickSendInvites = {onClickSendInvites}
+    />}
     </div>
   );
 }
